4eef14fe492cc47c4dd39be1f9ef16c229135d61
roshni090496 <rs09041001@gmail.com>
2022-04-15 19:05:35 -0700

    Leapyearprgm

---
+++ JavaAssignment/src/com/company/LeapYear.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the year");
+        int year = scn.nextInt();
+        boolean leap = false;
+
+
+        if (year % 4 == 0) {
+
+
+            if (year % 100 == 0) {
+
+
+                if (year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+
+
+            else
+                leap = true;
+        }
+
+        else
+            leap = false;
+
+        if (leap)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+
+}
